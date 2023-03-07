bb5c52a43fcb826640ebbf81fb12d6dcbc8315f0
Hassaan <f201092@cfd.nu.edu.pk>
2023-03-07 14:54:58 +0500

    hassaan commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 const product_cake = require("./routes/product-cakerouter")
 const product_cupcake = require("./routes/product-cupcakerouter")
 const contact = require("./routes/contactrouter")
@@ -9,16 +9,23 @@ const user_recentorders = require("./routes/user-recentordersrouter")
 const user_cart = require("./routes/user-cartrouter")
 const admin_product = require("./routes/admin-productrouter")
 const admin_addproduct = require("./routes/admin-addproductrouter")
+const home = require("./routes/homerouter")
+const user_profile = require("./routes/user-profilerouter")
+const admin_updateproduct = require("./routes/admin-updateproductrouter")
 
+
+app.use('/',home)
 app.use('/Products',product_cake)
 app.use('/Products',product_cupcake)
+
 app.use(contact)
 app.use(login)
 app.use('/User',user_recentorders)
 app.use('/User',user_cart)
-app.use('/Admin',admin_product)
+app.use('/User',user_profile)
+app.use('/Admin/Product',admin_product)
 app.use('/Admin/Product',admin_addproduct)
-
+app.use('/Admin/Product',admin_updateproduct)
 app.listen(port,()=>{
-    console.log('Server runing on 3000')
+    console.log('Server runing on 8000')
 })
